79e8eab37954e4523a9253e7d879ac04b95c3b3c
I hate JAVA <tg90124@gmail.com>
2023-03-17 15:10:38 +0800

    First commit2023.03.17

---
+++ src/main/java/com/example/test_demo_test/TestDemoTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_demo_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDemoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDemoTestApplication.class, args);
+	}
+
+}
